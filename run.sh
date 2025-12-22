1eb3dc4a37cf02ba70dcf996c3454b302f72f4ea
FOLINK <1602347500@qq.com>
2025-12-22 01:42:10 +0000

    update run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+python main.py +model=tlob +dataset=fi_2010 hydra.job.chdir=False
+python main.py +model=mlplob +dataset=fi_2010 hydra.job.chdir=False
+python main.py +model=deeplob +dataset=fi_2010 hydra.job.chdir=False
+python main.py +model=binctabl +dataset=fi_2010 hydra.job.chdir=False
+python main.py +model=tlob +dataset=btc hydra.job.chdir=False
+python main.py +model=mlplob +dataset=btc hydra.job.chdir=False
+python main.py +model=deeplob +dataset=btc hydra.job.chdir=False
+python main.py +model=binctabl +dataset=btc hydra.job.chdir=False
